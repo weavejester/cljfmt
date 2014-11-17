1b65b5119a52087c6380fd354bb56e88f910245f
James Reeves <jreeves@weavejester.com>
2014-11-17 01:49:52 +0000

    Add function to remove trailing newlines from a form

--- src/canon/core.clj
+++ src/canon/core.clj
@@ -1,7 +1,12 @@
 (ns canon.core
-  (:require [rewrite-clj.parser :as p]))
+  (:require [rewrite-clj.parser :as p]
+            [rewrite-clj.printer :as prn]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- form? [type? form]
+  (and (vector? form) (type? (first form))))
+
+(defn remove-trailing-newlines [form]
+  (->> (reverse form)
+       (drop-while (partial form? #{:whitespace :newline}))
+       (reverse)
+       (vec)))
