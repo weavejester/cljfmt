53cf8d4e880ec173ec30ffcb99c45a7f2513b4e8
James Reeves <jreeves@weavejester.com>
2014-11-17 23:19:20 +0000

    Add function to indent a collection

--- src/canon/core.clj
+++ src/canon/core.clj
@@ -3,10 +3,39 @@
             [rewrite-clj.printer :as prn]))
 
 (defn- form? [type? form]
-  (and (vector? form) (type? (first form))))
+  (boolean (and (vector? form) (type? (first form)))))
+
+(defn- lines [form]
+  (partition-by (partial form? #{:newline}) form))
+
+(defn- unlines [lines]
+  (vec (apply concat lines)))
+
+(defn- indentation [line]
+  (->> (take-while (partial form? #{:whitespace}) line)
+       (map (comp count second))
+       (apply +)))
+
+(defn- make-whitespace [indent]
+  [:whitespace (apply str (repeat indent " "))])
+
+(defn- indent-line [n line]
+  (vec (cons (make-whitespace n) line)))
+
+(defn- unindent-line [line]
+  (drop-while (partial form? #{:whitespace}) line))
 
 (defn remove-trailing-newlines [form]
   (->> (reverse form)
        (drop-while (partial form? #{:whitespace :newline}))
        (reverse)
        (vec)))
+
+(defn indent-collection
+  ([form] (indent-collection form 0))
+  ([form indent]
+     (let [[head & tail] (lines form)]
+       (->> (map unindent-line tail)
+            (map (partial indent-line (inc indent)))
+            (cons head)
+            (unlines)))))
