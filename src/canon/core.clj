86dd1f206fc7ee961c21e41ca0289b3ce35d8b2e
James Reeves <jreeves@weavejester.com>
2014-11-18 01:26:12 +0000

    Add recursive collection indentation

--- src/canon/core.clj
+++ src/canon/core.clj
@@ -11,31 +11,45 @@
 (defn- unlines [lines]
   (vec (apply concat lines)))
 
-(defn- indentation [line]
-  (->> (take-while (comp #{:whitespace} tag) line)
-       (map (comp count second))
-       (apply +)))
+(defn- make-whitespace [width]
+  [:whitespace (apply str (repeat width " "))])
 
-(defn- make-whitespace [indent]
-  [:whitespace (apply str (repeat indent " "))])
-
-(defn- indent-line [n line]
-  (vec (cons (make-whitespace n) line)))
+(defn- indent-line [margin line]
+  (vec (cons (make-whitespace margin) line)))
 
 (defn- unindent-line [line]
   (drop-while (comp #{:whitespace} tag) line))
 
-(defn remove-trailing-newlines [form]
+(defn- remove-trailing-newlines [form]
   (->> (reverse form)
        (drop-while (comp #{:whitespace :newline} tag))
        (reverse)
        (vec)))
 
-(defn indent-collection
-  ([form] (indent-collection form 0))
-  ([form indent]
-     (let [[head & tail] (lines form)]
-       (->> (map unindent-line tail)
-            (map (partial indent-line (inc indent)))
-            (cons head)
-            (unlines)))))
+(defn- element-width [elem]
+  (if (vector? elem)
+    (-> elem prn/->string count)
+    (-> [elem] prn/->string count dec)))
+
+(defn- position [line index]
+  (->> (take index line)
+       (map element-width)
+       (apply +)))
+
+(defn- indent-line-forms [margin line]
+  (map-indexed (fn [i x] (indent x (+ margin (position line i)))) line))
+
+(defn- indent-collection [form margin]
+  (let [[head & tail] (lines form)]
+    (->> (map unindent-line tail)
+         (map (partial indent-line (inc margin)))
+         (cons head)
+         (map (partial indent-line-forms margin))
+         (unlines))))
+
+(defn indent
+  ([form] (indent form 0))
+  ([form margin]
+     (condp contains? (tag form)
+       #{:vector :map :set} (indent-collection form margin)
+       form)))
