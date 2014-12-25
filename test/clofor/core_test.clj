70ff56d1037002617470808c37eedd2fa7a8e5ba
James Reeves <jreeves@weavejester.com>
2014-12-25 17:10:29 +0000

    Add indent tests

--- test/clofor/core_test.clj
+++ test/clofor/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [clofor.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-indent
+  (testing "function indentation"
+    (is (= (reformat-string "(foo bar\nbaz\nquz)")
+           "(foo bar\n     baz\n     quz)"))
+    (is (= (reformat-string "(foo\nbar\nbaz)")
+           "(foo\n bar\n baz)")))
+
+  (testing "block indentation"
+    (is (= (reformat-string "(if (= x 1)\n:foo\n:bar)")
+           "(if (= x 1)\n  :foo\n  :bar)"))
+    (is (= (reformat-string "(do\n(foo)\n(bar))")
+           "(do\n  (foo)\n  (bar))"))
+    (is (= (reformat-string "(do (foo)\n(bar))")
+           "(do (foo)\n    (bar))"))))
