e5fa44e22d363dab1dd459a168c9fb73589b3c86
James Reeves <jreeves@weavejester.com>
2014-12-25 17:27:56 +0000

    Add tests for surrounding whitespace

--- test/clofor/core_test.clj
+++ test/clofor/core_test.clj
@@ -16,3 +16,20 @@
            "(do\n  (foo)\n  (bar))"))
     (is (= (reformat-string "(do (foo)\n(bar))")
            "(do (foo)\n    (bar))"))))
+
+(deftest test-surrounding-whitespace
+  (testing "surrounding spaces"
+    (is (= (reformat-string "( foo bar )")
+           "(foo bar)"))
+    (is (= (reformat-string "[ 1 2 3 ]")
+           "[1 2 3]"))
+    (is (= (reformat-string "{  :x 1, :y 2 }")
+           "{:x 1, :y 2}")))
+
+  (testing "surrounding newlines"
+    (is (= (reformat-string "(\n  foo\n)")
+           "(foo)"))
+    (is (= (reformat-string "[\n1 2 3\n]")
+           "[1 2 3]"))
+    (is (= (reformat-string "{\n:foo \"bar\"\n}")
+           "{:foo \"bar\"}"))))
