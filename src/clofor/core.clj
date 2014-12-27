4051774302670f3f5b2f8e2d2c4e1fbcf60ca812
James Reeves <jreeves@weavejester.com>
2014-12-27 21:48:30 +0000

    Refactor default indent data into edn resource

--- src/clofor/core.clj
+++ src/clofor/core.clj
@@ -1,5 +1,7 @@
 (ns clofor.core
-  (:require [fast-zip.core :as fz]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [fast-zip.core :as fz]
             [rewrite-clj.parser :as p]
             [rewrite-clj.printer :as prn]
             [rewrite-clj.zip :as z]))
@@ -88,35 +90,28 @@
       (z/linebreak? zloc))
     true))
 
-(def block-indent-size 2)
+(def indent-size 2)
 
 (defn block-indent [zloc sym idx]
   (if (and (= (-> zloc fz/leftmost z/value) sym)
            (first-form-in-line? (nth-form zloc (inc idx)))
            (> (index-of zloc) idx))
-    (-> zloc z/up margin (+ block-indent-size))))
-
-(def block-indent-defaults
-  '{go   0, case   1, if-let   1, when-not        1
-    do   0, while  1, thread   0, defstruct       1
-    if   1, doseq  1, testing  1, with-open       1
-    ns   1, alt!!  0, finally  0, defrecord       2
-    are  1, letfn  1, deftype  2, when-some       1
-    try  0, condp  2, if-some  1, when-first      1
-    let  1, proxy  2, go-loop  1, struct-map      1
-    for  1, catch  2, dotimes  1, extend-type     1
-    doto 1, assoc  1, binding  1, defprotocol     1
-    alt! 0, if-not 1, locking  1, with-precision  1
-    when 1, future 0, comment  0, extend-protocol 1
-    loop 1, extend 1, when-let 1, with-local-vars 1})
+    (-> zloc z/up margin (+ indent-size))))
 
 (def default-indents
-  (for [[s i] block-indent-defaults] #(block-indent % s i)))
+  (edn/read-string (slurp (io/resource "clofor/indents.edn"))))
+
+(defmulti indent-fn
+  (fn [[sym [type & args]]] type))
+
+(defmethod indent-fn :block [[sym [_ idx]]]
+  (fn [zloc] (block-indent zloc sym idx)))
 
 (defn- indent-amount [zloc indents]
-  (if (-> zloc z/up z/tag #{:list})
-    (or ((apply some-fn indents) zloc) (list-indent zloc))
-    (coll-indent zloc)))
+  (let [indent-fn (apply some-fn (map indent-fn indents))]
+    (if (-> zloc z/up z/tag #{:list})
+      (or (indent-fn zloc) (list-indent zloc))
+      (coll-indent zloc))))
 
 (defn- indent-line [zloc indents]
   (fz/insert-left zloc (whitespace (indent-amount zloc indents))))
