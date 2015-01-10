ed2cd869a3556823cbeab636d6863a5b26fbecfa
James Reeves <jreeves@weavejester.com>
2015-01-10 13:16:14 +0000

    Add plugin project

---
+++ plugin/project.clj
@@ -0,0 +1,7 @@
+(defproject lein-cljfmt "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true
+  :dependencies [[cljfmt "0.1.0-SNAPSHOT"]])
---
+++ plugin/src/leiningen/format.clj
@@ -0,0 +1,32 @@
+(ns leiningen.format
+  (:refer-clojure :exclude [format])
+  (:require [cljfmt.core :as cljfmt]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [leiningen.core.main :as main]))
+
+(defn clojure-file? [file]
+  (re-find #"\.clj[sx]?" (str file)))
+
+(defn clojure-files [dir]
+  (filter clojure-file? (file-seq (io/file dir))))
+
+(defn source-files [project]
+  (mapcat clojure-files (:source-paths project)))
+
+(defn valid-format? [file]
+  (let [s (slurp file)]
+    (= (str/trim s) (cljfmt/reformat-string s))))
+
+(defn check [project]
+  (let [files (source-files project)]
+    (if (every? valid-format? files)
+      (println "ok")
+      (println "fail"))))
+
+(defn format
+  "Format Clojure source files"
+  [project command]
+  (case command
+    "check" (check project)
+    (main/abort "Unknown format command:" command)))
