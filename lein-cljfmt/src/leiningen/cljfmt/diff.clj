400a94b986a7ae4b34aca0b4f5529c4b78d45ccc
James Reeves <jreeves@weavejester.com>
2015-01-15 19:37:23 +0000

    Add line-based diff translation

--- lein-cljfmt/src/leiningen/cljfmt/diff.clj
+++ lein-cljfmt/src/leiningen/cljfmt/diff.clj
@@ -12,9 +12,19 @@
 
 (defn diff [a b]
   (let [patch (DiffUtils/diff (lines a) (lines b))]
-    (for [delta (.getDeltas patch)]
-      [(-> delta .getType delta-types)
-       (-> delta .getOriginal .getPosition)
-       (-> delta .getOriginal .getLines seq)
-       (-> delta .getRevised  .getLines seq)])))
+    (for [d (.getDeltas patch)]
+      {:type     (-> d .getType delta-types)
+       :line     (-> d .getOriginal .getPosition)
+       :original (-> d .getOriginal .getLines seq)
+       :revised  (-> d .getRevised  .getLines seq)})))
 
+(defn line-diff [{:keys [type line original revised] :as delta}]
+  (case type
+    :insert (map (fn [num content] [:insert num content])
+                 (range line (+ line (count revised)))
+                 revised)
+    :delete (map (fn [num content] [:delete num content])
+                 (range line (+ line (count original)))
+                 original)
+    :change (concat (line-delta (assoc delta :type :delete))
+                    (line-delta (assoc delta :type :insert)))))
