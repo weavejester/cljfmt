10d35fc5aeff3f0d1a76e77be5afdc529e99c797
James Reeves <jreeves@weavejester.com>
2015-01-15 20:21:04 +0000

    Better diff line representation

--- lein-cljfmt/src/leiningen/cljfmt/diff.clj
+++ lein-cljfmt/src/leiningen/cljfmt/diff.clj
@@ -10,21 +10,27 @@
    Delta$TYPE/DELETE :delete
    Delta$TYPE/INSERT :insert})
 
-(defn diff [a b]
-  (let [patch (DiffUtils/diff (lines a) (lines b))]
+(defn chunk->map [chunk]
+  (let [lines (vec (.getLines chunk))
+        start (.getPosition chunk)]
+    {:start start
+     :end   (+ start (count lines))
+     :lines lines}))
+
+(defn diff [original revised]
+  (let [patch (DiffUtils/diff (lines original) (lines revised))]
     (for [d (.getDeltas patch)]
       {:type     (-> d .getType delta-types)
-       :line     (-> d .getOriginal .getPosition)
-       :original (-> d .getOriginal .getLines seq)
-       :revised  (-> d .getRevised  .getLines seq)})))
+       :original (some-> d .getOriginal chunk->map)
+       :revised  (some-> d .getRevised chunk->map)})))
 
-(defn line-diff [{:keys [type line original revised] :as delta}]
+(defn line-diff [{:keys [type original revised] :as delta}]
   (case type
     :insert (map (fn [num content] [:insert num content])
-                 (range line (+ line (count revised)))
-                 revised)
+                 (range (:start revised) (:end revised))
+                 (:lines revised))
     :delete (map (fn [num content] [:delete num content])
-                 (range line (+ line (count original)))
-                 original)
-    :change (concat (line-delta (assoc delta :type :delete))
-                    (line-delta (assoc delta :type :insert)))))
+                 (range (:start original) (:end original))
+                 (:lines original))
+    :change (concat (line-diff (assoc delta :type :delete))
+                    (line-diff (assoc delta :type :insert)))))
