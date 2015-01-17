acf190e0ded5864a2919244334a9061e425885cf
James Reeves <jreeves@weavejester.com>
2015-01-17 13:43:05 +0000

    Add description, URL and SCM path to project files

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,6 +1,7 @@
 (defproject cljfmt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library for formatting Clojure code"
+  :url "https://github.com/weavejester/cljfmt"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
--- lein-cljfmt/project.clj
+++ lein-cljfmt/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-cljfmt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library for formatting Clojure code"
+  :url "https://github.com/weavejester/cljfmt"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
