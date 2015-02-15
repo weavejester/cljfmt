3cc8a8927f5971f0e85886d59676f32d7c4b7cfb
James Reeves <jreeves@weavejester.com>
2015-02-15 16:07:11 +0000

    Release 0.1.9

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,4 +1,4 @@
-(defproject cljfmt "0.1.8"
+(defproject cljfmt "0.1.9"
   :description "A library for formatting Clojure code"
   :url "https://github.com/weavejester/cljfmt"
   :scm {:dir ".."}
--- lein-cljfmt/project.clj
+++ lein-cljfmt/project.clj
@@ -1,9 +1,9 @@
-(defproject lein-cljfmt "0.1.8"
+(defproject lein-cljfmt "0.1.9"
   :description "A library for formatting Clojure code"
   :url "https://github.com/weavejester/cljfmt"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[cljfmt "0.1.8"]
+  :dependencies [[cljfmt "0.1.9"]
                  [com.googlecode.java-diff-utils/diffutils "1.2.1"]])
