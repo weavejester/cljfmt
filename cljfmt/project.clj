624404e36b6aee44dd84f274703bfa8e2c31d607
James Reeves <jreeves@weavejester.com>
2016-02-23 16:54:04 +0000

    Make ClojureScript dependency 'provided'

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -5,7 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
                  [rewrite-clj "0.4.12"]
                  [rewrite-cljs "0.4.0"]]
   :plugins [[lein-cljsbuild "1.1.2"]]
@@ -18,4 +17,6 @@
                                  :target :nodejs
                                  :optimizations :none}}}
               :test-commands
-              {"dev" ["node" "target/out/tests.js"]}})
+              {"dev" ["node" "target/out/tests.js"]}}
+  :profiles
+  {:provided {:dependencies [[org.clojure/clojurescript "1.7.228"]]}})
