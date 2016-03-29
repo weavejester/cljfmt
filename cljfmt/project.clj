74a549cff38532a1c440896e4abff4163108ad06
James Reeves <jreeves@weavejester.com>
2016-03-29 02:48:20 +0100

    Update version of tools.reader

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.reader "1.0.0-alpha3"]
+                 [org.clojure/tools.reader "1.0.0-alpha4"]
                  [rewrite-clj "0.4.12"]
                  [rewrite-cljs "0.4.1"]]
   :plugins [[lein-cljsbuild "1.1.2"]]
