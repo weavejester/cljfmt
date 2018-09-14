8533da53d5463bfeb78a791d5a60fd6b03dab7fe
James Reeves <jreeves@weavejester.com>
2018-09-14 10:35:10 +0100

    Update ClojureScript deps to work with Java 10

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -4,13 +4,13 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.7"]
                  [org.clojure/tools.reader "1.2.2"]
                  [com.googlecode.java-diff-utils/diffutils "1.3.0"]
                  [rewrite-clj "0.6.0"]
                  [rewrite-cljs "0.4.4"]]
-  :plugins [[lein-cljsbuild "1.1.2"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds
               {"dev" {:source-paths ["src" "test"]
@@ -22,4 +22,4 @@
               :test-commands
               {"dev" ["node" "target/out/tests.js"]}}
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "1.7.228"]]}})
+  {:provided {:dependencies [[org.clojure/clojurescript "1.10.339"]]}})
