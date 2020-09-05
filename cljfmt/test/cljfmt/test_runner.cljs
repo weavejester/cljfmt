b987d06263fdb8825b4206b7ffa7546832723ad3
James Reeves <jreeves@weavejester.com>
2020-09-05 16:51:11 +0100

    Fix test runner exit code

--- cljfmt/test/cljfmt/test_runner.cljs
+++ cljfmt/test/cljfmt/test_runner.cljs
@@ -5,6 +5,10 @@
 
 (nodejs/enable-util-print!)
 
+(defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
+  (when (pos? (:fail m))
+    (js/process.exit 1)))
+
 (defn -main []
   (run-tests 'cljfmt.core-test))
 
