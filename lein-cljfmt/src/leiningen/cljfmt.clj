f3fd2a4ac7e333a50a2621b9be48296d388ca24e
James Reeves <jreeves@weavejester.com>
2023-05-19 18:26:41 +0100

    Fix lein-cljfmt plugin

--- lein-cljfmt/src/leiningen/cljfmt.clj
+++ lein-cljfmt/src/leiningen/cljfmt.clj
@@ -1,37 +1,37 @@
 (ns leiningen.cljfmt
-  (:require [cljfmt.core :as cljfmt]
-            [cljfmt.main :as main]
+  (:require [cljfmt.config :as config]
+            [cljfmt.tool :as tool]
             [clojure.java.io :as io]
-            [leiningen.core.main]))
+            [leiningen.core.main :as lein]))
 
 (defn- format-paths [project]
   (let [paths (concat (:source-paths project)
                       (:test-paths project))]
     (if (empty? paths)
-      (leiningen.core.main/abort "No source or test paths defined in project map")
+      (lein/abort "No source or test paths defined in project map")
       (->> (map io/file paths)
            (filter #(and (.exists %) (.isDirectory %)))))))
 
-(defn- execute-command [command options paths]
+(defn- execute-command [command options]
   (case command
-    "check" (cljfmt.main/check paths options)
-    "fix"   (cljfmt.main/fix paths options)
-    (leiningen.core.main/abort "Unknown cljfmt command:" command)))
+    "check" (tool/check-no-config options)
+    "fix"   (tool/fix-no-config options)
+    (lein/abort "Unknown cljfmt command:" command)))
 
 (defn- merge-default-options [options]
-  (main/merge-options main/default-options options))
+  (config/merge-configs config/default-config options))
 
 (defn ^:no-project-needed cljfmt
-  "Format Clojure source files"
+  "Format Clojure source files."
   [project command & paths]
   (let [paths   (or (seq paths)
                     (-> project :cljfmt :paths)
                     (format-paths project))
         options (-> (:cljfmt project)
-                    (dissoc :paths)
+                    (assoc :paths paths)
                     (assoc :project-root (:root project))
                     (merge-default-options))]
     (if leiningen.core.main/*info*
-      (execute-command command options paths)
+      (execute-command command options)
       (with-out-str
-        (execute-command command options paths)))))
+        (execute-command command options)))))
