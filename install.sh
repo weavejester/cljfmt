280608402e43573295e0b6bf64e096b70cfb4fb0
James Reeves <jreeves@weavejester.com>
2023-05-21 23:26:36 +0100

    Add script for installing binaries

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VERSION=0.10.2
+
+case $(uname -s) in
+    Linux*)
+        PLATFORM=linux
+        ;;
+    Darwin*)
+        PLATFORM=darwin
+        ;;
+esac
+
+case $(uname -m) in
+    aarch64)
+        ARCH=aarch64
+        ;;
+    arm64)
+        ARCH=aarch64
+        ;;
+    *)
+        ARCH=amd64
+        if [[ "$PLATFORM" == "linux" ]]; then
+            STATIC="-static"
+        fi
+        ;;
+esac
+
+BASE_URL="https://github.com/weavejester/cljfmt/releases/download"
+URL="${BASE_URL}/${VERSION}/cljfmt-${VERSION}-${PLATFORM}-${ARCH}${STATIC}.tar.gz"
+
+echo -n "Downloading cljfmt binaries... "
+curl -o /tmp/cljfmt.tar.gz -sL "$URL"
+echo "Done!"
+
+sudo tar -xzf /tmp/cljfmt.tar.gz -C /usr/local/bin
+echo "Extracted cljfmt into /usr/local/bin"
