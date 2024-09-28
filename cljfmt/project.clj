d5a58f2051552ebcd90710b0518a3deb2a31b3fc
James Reeves <jreeves@weavejester.com>
2024-09-28 19:19:01 +0100

    Release 0.13.0

--- cljfmt/project.clj
+++ cljfmt/project.clj
@@ -1,4 +1,4 @@
-(defproject dev.weavejester/cljfmt "0.12.0"
+(defproject dev.weavejester/cljfmt "0.13.0"
   :description "A library for formatting Clojure code"
   :url "https://github.com/weavejester/cljfmt"
   :scm {:dir ".."}
